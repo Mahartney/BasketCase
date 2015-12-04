759b05ce4c09b487deabfae7864cfb62ebc67a24
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-12-04 14:03:55 -0500

    spec folder created

---
+++ spec/basket-spec.js
@@ -0,0 +1,29 @@
+describe ("a basket", function(){
+  it ("should be an object", function(){
+
+  });
+
+  it ("should have an array of items", function(){
+
+  });
+
+  it ("should have a budget", function(){
+
+  });
+
+  it ("should have a total cost", function(){
+
+  });
+
+  describe ("a basket created by a registered user", function(){
+    it ("should have a creator", function(){
+
+    });
+  });
+  
+});
+
+  // it should have items
+  // it should have a budget
+  // it should have a total cost
+  // it should have a user if it is created by a user
