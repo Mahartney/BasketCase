884e126127fd4f6a979661df341b1e3c21c6143f
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-12-05 16:14:06 -0500

    added and configured passport. added user model

---
+++ controllers/usersController.js
@@ -0,0 +1,39 @@
+var passport = require("passport")
+
+// GET /signup
+var usersController = {
+  getSignup: function(req, res){
+    res.render('signup', {message: req.flash('signupMessage')
+    });
+  },
+
+   postSignup: function(req, res){
+    var signupStrategy = passport.authenticate('local-signup', {
+      successRedirect: '/',
+      failureRedirect: '/signup',
+      failureFlash: true
+    });
+    return signupStrategy(req, res);
+  },
+
+   getLogin: function(req, res){
+    res.render('login.hbs', {message: req.flash('loginMessage')})
+  },
+
+   postLogin: function(req, res){
+    var loginStrategy = passport.authenticate('local-login', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true
+    });
+    return loginStrategy(req, res);
+  },
+
+   getLogout: function(req, res){
+    req.logout();
+    res.redirect('/');
+  }
+}
+
+
+module.exports = usersController;
--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var apac = require('apac');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session      = require('express-session');
 var methodOverride = require('method-override')
-
+var usersController = require('./controllers/usersController')
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -12,10 +15,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+app.use(passport.initialize());
+app.use(session({secret: 'Basket Case'}));
+app.use(passport.session());
+app.use(flash());
+require('./config/passport')(passport);
 
 app.get('/', function(req, res){
   res.render('index.hbs');
 });
+
+app.use(function(req, res, next){
+  global.currentUser = req.user;
+  res.locals.currentUser = req.user
+  next();
+})
+
+app.get('/signup', usersController.getSignup);
+app.post('/signup', usersController.postSignup);
+app.get('/login', usersController.getLogin);
+app.post('/login', usersController.postLogin);
+app.get('/logout', usersController.getLogout);
 
 app.listen(3000, function(){
   console.log('Listening on port 3000');
