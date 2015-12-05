a2ac370759227973be82627f138f5f7da09ecf16
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-12-05 16:29:00 -0500

    bug fixes for user authentication

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var apac = require('apac');
+var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
 var session      = require('express-session');
 var methodOverride = require('method-override')
 var usersController = require('./controllers/usersController')
 app.set('view engine', 'hbs');
+
+mongoose.connect('mongodb://localhost/basketcase');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
-app.use(passport.initialize());
 app.use(session({secret: 'Basket Case'}));
+app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 require('./config/passport')(passport);
@@ -30,6 +34,13 @@ app.use(function(req, res, next){
   res.locals.currentUser = req.user
   next();
 })
+
+function authenticatedUser(req, res, next){
+  if(req.isAuthenticated()){
+    return next()
+  }
+  res.redirect('/')
+}
 
 app.get('/signup', usersController.getSignup);
 app.post('/signup', usersController.postSignup);
