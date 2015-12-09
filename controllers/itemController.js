f8529e1b6142f3ceb8874dd325274341da694c83
Davis Green <davismgreen@gmail.com>
2015-12-09 12:26:26 -0500

    changes price of offers price

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -20,20 +20,35 @@ var APICall = function(res){
   opHelper.execute('ItemSearch', {
     'SearchIndex': 'All',
     'Keywords': '*',
-    'ResponseGroup': 'ItemAttributes,Images,Offers,Variations',
-    'MinimumPrice': '1'
+    'ResponseGroup': 'ItemAttributes,Images,Offers,OfferFull,OfferSummary',
+    'MinimumPrice': 2585,
+    'MaximumPrice': 2585,
+    'MerchandID': 'All'
   }, function(err, results) {
     console.log("error: " + err);
     console.log("the next line is results: ")
     //console.log(results);
     var newItem = itemController.createItem()
-    var item = results["ItemSearchResponse"]["Items"][0]["Item"][0]["ItemAttributes"][0]
-  //  newItem.price = item["ItemAttributes"][0]["ListPrice"][0]["Amount"][0]
-  //  newItem.name = item["ItemAttributes"][0]["Title"][0]
-  //  newItem.thumbnail = item["SmallImage"][0]["URL"][0]
-  //  newItem.image = item["MediumImage"][0]["URL"][0]
-    console.log(newItem)
-    console.log(item)
+    var findItem = 0
+    var returnArr = results["ItemSearchResponse"]["Items"][0]["Item"]
+
+    for (var i = 0; i < returnArr.length; i++) {
+      if (returnArr[i].hasOwnProperty('Offers')) {
+        if (Number(returnArr[i]["Offers"][0]["TotalOffers"][0])>0) {
+          findItem = i
+          break
+        }
+      }
+    }
+    console.log(findItem)
+
+    var item = results["ItemSearchResponse"]["Items"][0]["Item"][findItem]
+    newItem.price = Number(item["Offers"][0]["Offer"][0]["OfferListing"][0]["Price"][0]["Amount"][0])
+    newItem.name = item["ItemAttributes"][0]["Title"][0]
+    newItem.thumbnail = item["SmallImage"][0]["URL"][0]
+    newItem.image = item["MediumImage"][0]["URL"][0]
+    console.log("results: "+ newItem)
+    //return newItem;
     return res.json(results);
   });
 }
@@ -60,10 +75,18 @@ var itemController = {
   amazonCall: function(req, res){
     // var qwer = APICall();
     // console.log("this is what qwer is " + qwer);
+
     var basket = basketController.createBasket()
+
     var getItem = APICall(res);
+
+    //console.log(getItem)
+
     basket.items.push(getItem)
+
   }
+
+
 }
 
 module.exports = itemController
