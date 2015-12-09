b493c8e1266f3a2c20b6bbf37bacb826c171d762
Davis Green <davismgreen@gmail.com>
2015-12-09 16:00:22 -0500

    a sync loop feature

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -16,18 +16,16 @@ var opHelper = new OperationHelper({
   assocId: env.assocId,
 });
 
-var APICall = function(res){
+var APICall = function(newBasket, i){
   opHelper.execute('ItemSearch', {
     'SearchIndex': 'All',
     'Keywords': '*',
     'ResponseGroup': 'ItemAttributes,Images,Offers,OfferFull,OfferSummary',
-    'MinimumPrice': 2585,
-    'MaximumPrice': 2585,
+    'MinimumPrice': i-100,
+    'MaximumPrice': i,
     'MerchandID': 'All'
   }, function(err, results) {
     console.log("error: " + err);
-    console.log("the next line is results: ")
-    //console.log(results);
     var newItem = itemController.createItem()
     var findItem = 0
     var returnArr = results["ItemSearchResponse"]["Items"][0]["Item"]
@@ -40,7 +38,6 @@ var APICall = function(res){
         }
       }
     }
-    console.log(findItem)
 
     var item = results["ItemSearchResponse"]["Items"][0]["Item"][findItem]
     newItem.price = Number(item["Offers"][0]["Offer"][0]["OfferListing"][0]["Price"][0]["Amount"][0])
@@ -48,9 +45,13 @@ var APICall = function(res){
     newItem.thumbnail = item["SmallImage"][0]["URL"][0]
     newItem.image = item["MediumImage"][0]["URL"][0]
     newItem.amazonUrl = item["ItemLinks"][0]["ItemLink"][0]["URL"][0]
-    console.log("results: "+ newItem)
-    return newItem;
+    //console.log("results: "+ newItem)
+    newBasket.items.push(newItem);
     //return res.json(results);
+    if (newBasket.items.length == newBasket.rnd_budgets.length) {
+      res.redirect("/basket/"+newBasket.id)
+    }
+    console.log(newBasket)
   });
 }
 
@@ -72,23 +73,12 @@ var itemController = {
     return item;
   },
 
-  amazonCall: function(newBasket){
+  amazonCall: function(newBasket, callback){
     console.log(newBasket)
-    var i = 0
-    pizza = true
-    while(pizza){
-      if (newBasket.items.length == newBasket.rnd_budgets.length) {
-        pizza = false;
-        break
-      }
-      while(i < newBasket.rnd_budgets.length) {
-        newBasket.items.push(APICall(newBasket.rnd_budgets[i]))
-        i++
-        }
-      console.log("Hello")
+    for (var i = 0; i < newBasket.rnd_budgets.length; i++) {
+      APICall(newBasket, newBasket.rnd_budgets[i])
     }
-
-  }//.next(DOM MANIPULATION)
+  }
 
 
 }
