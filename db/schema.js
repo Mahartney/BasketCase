a1c055de9f1e710e74c882dcd040fc2ca0148df4
Peter Zhang <pzhang87@gmail.com>
2015-12-10 10:55:14 -0500

    mongoose to connect to 127.0.0.1 for db

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/basketcase');
+mongoose.connect('mongodb://127.0.0.1:27017/basketcase');
 
 var Schema = mongoose.Schema,
     ObjectId = Schema.Types.ObjectId
