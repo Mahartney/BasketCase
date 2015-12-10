e691447e7591b44d3ca4298c72220ccc0a76ac52
Peter Zhang <pzhang87@gmail.com>
2015-12-10 11:43:46 -0500

    remove getItems method on seeds in basket

--- db/seeds.js
+++ db/seeds.js
@@ -22,10 +22,7 @@ ItemModel.remove({}, function(err){
     value: 0,
     liked: false,
     items: [],
-    // method: getItem(){
-    //
-    // }
-  })
+  });
   basket1.save();
 
   var basket2 = new BasketModel({
@@ -33,7 +30,7 @@ ItemModel.remove({}, function(err){
     value: 0,
     liked: false,
     items: [],
-  })
+  });
   basket2.save();
 
   var basket3 = new BasketModel({
@@ -41,7 +38,7 @@ ItemModel.remove({}, function(err){
     value: 0,
     liked: false,
     items: [],
-  })
+  });
   basket3.save();
 
   var item1 = new ItemModel({
@@ -50,7 +47,7 @@ ItemModel.remove({}, function(err){
     thumbnail: "",
     image: "https://www.organicfacts.net/wp-content/uploads/2013/05/Banana21.jpg",
     description: "A 5 Dollar Thing"
-  })
+  });
   item1.save();
 
   var item2 = new ItemModel({
@@ -59,7 +56,7 @@ ItemModel.remove({}, function(err){
     thumbnail: "",
     image: "http://www.plasticstoday.com/sites/default/files/Red-Solo-Cup.jpg",
     description: "A 10 Dollar Thing"
-  })
+  });
   item2.save();
 
   var item3 = new ItemModel({
@@ -68,7 +65,7 @@ ItemModel.remove({}, function(err){
     thumbnail: "",
     image: "http://www.gadgetreview.com/wp-content/uploads/2014/04/STREAMZ-01.jpg",
     description: "A 23 Dollar Thing"
-  })
+  });
   item3.save();
 
   var item4 = new ItemModel({
@@ -77,7 +74,7 @@ ItemModel.remove({}, function(err){
     thumbnail: "",
     image: "http://media4.popsugar-assets.com/files/2015/09/15/878/n/1922153/6de294e7a93c869c_0GQ-Screws/i/Screws.jpg",
     description: "A 3 Dollar Thing"
-  })
+  });
   item4.save();
 
   var item5 = new ItemModel({
@@ -86,7 +83,7 @@ ItemModel.remove({}, function(err){
     thumbnail: "",
     image: "http://www.imprentaonline.net/blog/wp-content/uploads/papel.jpg",
     description: "A 16 Dollar Thing"
-  })
+  });
   item5.save();
 
   // NK: Commenting this out because we want to test attaching items to baskets
