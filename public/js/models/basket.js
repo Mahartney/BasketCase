0ef5004fa41996d98d27c509dbc17efa0a4b2059
Davis Green <davismgreen@gmail.com>
2015-12-11 10:09:02 -0500

    pre-push commit"

--- public/js/models/basket.js
+++ public/js/models/basket.js
@@ -23,19 +23,26 @@ popMostRecent = function(){
     var html = $(".mostRecent")
     html.empty();
     for (var i = 0; i < response.length; i++) {
+
+      var thisBasket = response[i]
       var itemThumbnails = $("<p>");
       var li = $("<li>");
-      var header = $("<div class ='collapsible-header truncate'>"+ response[i].items[0].name + "</div>");
-      var body = $("<div class ='collapsible-body'>");
-      for (var j = 0; j < response[i].items.length; j++) {
-        itemThumbnails.append("<img class ='thumbnail' src='" + response[i].items[j].thumbnail + "'>")
+      var header = $("<div class ='collapsible-header truncate'>"+ thisBasket.items[0].name + "</div>");
+
+      var body = $("<div class ='collapsible-body'>").click(function(){
+        var view = new BasketView(thisBasket);
+        view.render();
+      });
+
+      for (var j = 0; j < thisBasket.items.length; j++) {
+        itemThumbnails.append("<img class ='thumbnail' src='" + thisBasket.items[j].thumbnail + "'>")
       };
       body.append(itemThumbnails)
       li.append(header);
       li.append(body);
       html.append(li);
     };
-    }).fail(function(response){
+  }).fail(function(response){
       console.log("2");
     });
   return request;
