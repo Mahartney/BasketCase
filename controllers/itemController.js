97a90a75aba59333cd17f392f9cb5e3c05234977
Peter Zhang <pzhang87@gmail.com>
2015-12-11 10:52:32 -0500

    enable env for master

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,19 +4,19 @@ var Basket = require("../models/basket");
 var Item = require("../models/item");
 var basketController = require("./basketController")
 var util = require('util');
-// var env = require('../env.js')
+var env = require('../env.js')
 OperationHelper = require('../node_modules/apac').OperationHelper;
 
 var randomWord = require("./helpers/keyword.js")
 
 
 var opHelper = new OperationHelper({
-  // awsId:     env.awsId,
-  // awsSecret: env.awsSecret,
-  // assocId:   env.assocId
-  awsId:     process.env.AWS_ID,
-  awsSecret: process.env.AWS_SECRET,
-  assocId:   process.env.ASSOC_ID
+  awsId:     env.awsId,
+  awsSecret: env.awsSecret,
+  assocId:   env.assocId
+  // awsId:     process.env.AWS_ID,
+  // awsSecret: process.env.AWS_SECRET,
+  // assocId:   process.env.ASSOC_ID
   }
 );
 
